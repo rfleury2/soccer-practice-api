8a1dd274044890285787bcfdb5030cda361cc527
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2015-12-10 13:23:32 -0600

    Add german, italian, dutch, and spanish leagues

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,6 @@
 
 require 'csv'
 
-# seed_england 
-seed_germany
-
-
 def seed_england
 	x = 0
 	puts "Loading English games"
@@ -20,7 +16,7 @@ def seed_england
 		x = x+1
 		Game.find_or_create_by(
 			date: Date.parse(date),
-			season: season,
+			season: season.to_s,
 			home_team: Team.find_or_create_by(name: home),
 			away_team: Team.find_or_create_by(name: visitor),
 			home_goals: hgoal.to_i,
@@ -30,3 +26,85 @@ def seed_england
 		)
 	end
 end
+
+def seed_italy
+	x = 0
+	puts "Loading Italian games"
+	CSV.foreach("vendor/raw_data/italycalcio.csv") do |date, season, home, visitor, ft, hgoal, vgoal, division|
+		puts "Loading game #{x} - #{home} vs #{visitor} - #{date}"
+		x = x+1
+		Game.find_or_create_by(
+			date: Date.parse(date),
+			season: season.to_s,
+			home_team: Team.find_or_create_by(name: home),
+			away_team: Team.find_or_create_by(name: visitor),
+			home_goals: hgoal.to_i,
+			away_goals: vgoal.to_i,
+			division: division,
+			league: 'Italy'
+		)
+	end
+end
+
+def seed_holland
+	x = 0
+	puts "Loading Dutch games"
+	CSV.foreach("vendor/raw_data/holland1.csv") do |date, season, home, visitor, ft, hgoal, vgoal, division|
+		puts "Loading game #{x} - #{home} vs #{visitor} - #{date}"
+		x = x+1
+		Game.find_or_create_by(
+			date: Date.parse(date),
+			season: season.to_s,
+			home_team: Team.find_or_create_by(name: home),
+			away_team: Team.find_or_create_by(name: visitor),
+			home_goals: hgoal.to_i,
+			away_goals: vgoal.to_i,
+			division: division,
+			league: 'Holland'
+		)
+	end
+end
+
+def seed_spain
+	x = 0
+	puts "Loading Spanish games"
+	CSV.foreach("vendor/raw_data/spainliga.csv") do |date, season, home, visitor, ft, hgoal, vgoal, division|
+		puts "Loading game #{x} - #{home} vs #{visitor} - #{date}"
+		x = x+1
+		Game.find_or_create_by(
+			date: Date.parse(date),
+			season: season.to_s,
+			home_team: Team.find_or_create_by(name: home),
+			away_team: Team.find_or_create_by(name: visitor),
+			home_goals: hgoal.to_i,
+			away_goals: vgoal.to_i,
+			division: division,
+			league: 'Spain'
+		)
+	end
+end
+
+def seed_germany
+	x = 0
+	puts "Loading German games"
+	CSV.foreach("vendor/raw_data/bundesliga.csv") do |date, season, home, visitor, ft, hgoal, vgoal, division|
+		puts "Loading game #{x} - #{home} vs #{visitor} - #{date}"
+		x = x+1
+		Game.find_or_create_by(
+			date: Date.parse(date),
+			season: season.to_s,
+			home_team: Team.find_or_create_by(name: home),
+			away_team: Team.find_or_create_by(name: visitor),
+			home_goals: hgoal.to_i,
+			away_goals: vgoal.to_i,
+			division: division,
+			league: 'Germany'
+		)
+	end
+end
+
+seed_england 
+seed_italy
+seed_holland
+seed_spain
+seed_germany
